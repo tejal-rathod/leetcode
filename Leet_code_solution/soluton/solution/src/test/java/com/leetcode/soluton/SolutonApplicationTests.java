eda7e8821ccfd436ccbe2ad8589229ac496bb9e4
Tejal Rathod <2023ad05045@wilp.bits-pilani.ac.in>
2026-02-28 20:45:35 +0530

    Day1 leetcode buy sell stock

---
+++ Leet_code_solution/soluton/solution/src/test/java/com/leetcode/soluton/SolutonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.soluton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
